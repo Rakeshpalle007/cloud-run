ed3dfc6753243223f1eeb0b59b6fa9248b390972
Rakeshpalle007 <rakeshpalle007@gmail.com>
2025-09-02 16:06:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+
+'use strict';
+
 const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
 const app = express();
-const PORT = process.env.PORT || 8080;
-
-// Simple route
 app.get('/', (req, res) => {
-  res.send('Hello from Cloud Run!');
+  res.send('Hello World, Hope you are doing well !! ****************************************************************************************');
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
